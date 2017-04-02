c6f7967368f45f009062ac5525ff3c8d47129e04
Speedlight <tao.ceelgmail.com>
2017-04-02 15:32:16 -0500

    TODO input tests.

--- src/rbls/models.py
+++ src/rbls/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 class Rbllist(models.Model):
     name = models.CharField(max_length=100)
-    url = models.CharField(max_length=100)
+    url = models.URLField(max_length=100, unique=True)
     description = models.CharField(max_length=300, null=True, blank=True)
 
     def __str__(self):
--- src/rbls/tests.py
+++ src/rbls/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Rbllist
 
-# Create your tests here.
+class RblListTest(TestCase):
+
+    def setUp(self):
+        self.obj01 = Rbllist(name='Test RBL 01', url='test1.abuse.org')
+        self.obj02 = Rbllist(name='Test RBL 02', url='test2.abuse.org')
+
+    def test_ip_input(self):
+        # "Test if the input is a valid ipv4 address"
+
+    def test_doamin_input(self):
+        # "Test if the input is a valid domain name"
